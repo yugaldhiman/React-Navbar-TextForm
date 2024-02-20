a83ec063ec0b0ce42d240b2f540adaa84bb2e03c
yugal dhiman <yugaldhiman14@gmail.com>
2024-02-20 17:15:43 +0530

    add new file

---
+++ arrayreverse.js
@@ -0,0 +1,11 @@
+const array = [2, 4, 6, 8, 10];
+array.reverse();
+console.log(array);
+
+const Name = ["rajan", "yugal", "himanshu", "aniket"];
+Name.reverse();
+console.log(Name);
+
+const string1 = "rajan yugal himanshu aniket";
+const string2 = string1.split("").reverse().join("");
+console.log(string2);
