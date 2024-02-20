2625b673226e5024990d8dd5742cf457f5d309a7
yugal dhiman <yugaldhiman14@gmail.com>
2024-02-20 14:11:10 +0530

    add if else

--- arrayincludes.js
+++ arrayincludes.js
@@ -1,6 +1,10 @@
-const fruit = ["apple","banana","orange"];
-const result1 = fruit.includes("banana");
-console.log(result1); 
-
-const result2 = fruit.includes("lemon");
+const fruit1 = ["apple", "banana", "orange"];
+const result2 = fruit1.includes("orange");
 console.log(result2);
+// const result2 = fruit.includes("lemon");
+// console.log(result2);
+if (result2) {
+  console.log("i am happy");
+} else {
+  console.log("i am not happy");
+}
